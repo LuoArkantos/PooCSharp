b55a8102cdd340cb6268206088886d17296861e9
LuoArkantos <luizfernandolima1997@gmail.com>
2023-01-18 20:23:53 -0300

    finalizando pt 1 poo

--- bytebank/ContaCorrente.cs
+++ bytebank/ContaCorrente.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,11 +9,28 @@ namespace bytebank
 {
     public class ContaCorrente
     {
-        public int agencia = 0000; //valores definido no obj são valores padroes
-        public string conta = "Sem Conta";
-        public double saldo = 100;
+        public static int TotalDeContas{ get; private set; } //Static significa q ela só pode ser usada dentro da classe, não do objeto. Dispensa o "this." / O PRIVATE SET significa que não pode ser atribuido valor manualmente pelo programa. Então TotalDeContas só devolve valor, não recebe nada manualmente, só quando incrementado por metodo ou propriedade.
 
-        public Cliente titular;
+        // Propriedade com validação
+        private int agencia = 0000; //valores definido no obj são valores padroes
+        public int Agencia
+        {
+            get { return this.agencia; }
+            private set
+            {
+                if (value > 0)
+                {
+                    this.agencia = value;
+                }
+            }
+        }
+
+        // Propriedade sem validaçao
+        public string Conta { get; set; } //Cria uma propriedade publica q manipula uma var privada internamente (propriedade autoimplementada)
+
+        private double saldo = 100;
+
+        public Cliente Titular;
 
         public void Depositar(double valor) // Método para adicionar valor (no programa principal, declaramos a variável como var.Depositar(valor) e inserimos o valor de depósito nos parentesis.
         {
@@ -43,18 +61,40 @@ namespace bytebank
                 this.Sacar(valor);
                 Destino.Depositar(valor);
                 Console.WriteLine();
-                Console.WriteLine($"Foi transferido R${valor} da conta de {this.titular} para a conta de {Destino.titular}");
+                Console.WriteLine($"Foi transferido R${valor} da conta de {this.Titular} para a conta de {Destino.Titular}");
                 return true;
             }
         }
 
         public void exibirInformações() // metodo para exibir dados dos clientes;
         {
-            Console.WriteLine("Titular :" + titular);
-            Console.WriteLine("Conta :" + conta);
+            Console.WriteLine("Titular :" + Titular);
+            Console.WriteLine("Conta :" + Conta);
             Console.WriteLine("Número Agência :" + agencia);
             Console.WriteLine("Saldo: " + saldo);
         }
 
+        public void SetSaldo(double valor)
+        {
+            if (valor < 0)
+            {
+                return;
+            }
+            else
+            {
+                this.saldo = valor;
+            }
+        }
+
+        public double GetSaldo()
+        {
+            return this.saldo;
+        }
+        public ContaCorrente(int numero_agencia, string numero_conta)
+        {
+            this.Agencia = numero_agencia;
+            this.Conta = numero_conta;
+            TotalDeContas++;
+        }
     }
 }
--- bytebank/Program.cs
+++ bytebank/Program.cs
@@ -1,80 +1,101 @@
 ﻿using bytebank;
 
-//// criando obj Andre
-//ContaCorrente contaDoAndre = new ContaCorrente();
+////// criando obj Andre
+////ContaCorrente contaDoAndre = new ContaCorrente();
 
-//contaDoAndre.titular = "André";
-//contaDoAndre.agencia = 15;
+////contaDoAndre.titular = "André";
+////contaDoAndre.agencia = 15;
+////contaDoAndre.conta = "1010-x";
+////contaDoAndre.saldo = 100;
+
+////Console.WriteLine($"Saldo Inicial de {contaDoAndre.titular} é R${contaDoAndre.saldo}");
+
+////// criando obj Maria
+////ContaCorrente contaDaMaria = new ContaCorrente();
+
+////contaDaMaria.titular = "Maria";
+////contaDaMaria.agencia = 17;
+////contaDaMaria.conta = "1010-5";
+////contaDaMaria.saldo = 350;
+
+////Console.WriteLine($"Saldo Inicial de {contaDaMaria.titular} é R${contaDaMaria.saldo}");
+
+////// Mtd para tranferir valores entre contas
+////contaDoAndre.Transferir(50, contaDaMaria);
+
+////Console.WriteLine($"Saldo final da conta do Andre: {contaDoAndre.saldo}");
+////Console.WriteLine($"Saldo final da conta do Andre: {contaDaMaria.saldo}");
+
+////// Para mostrar valores padrão do obj
+////ContaCorrente contaDoPedro = new ContaCorrente();
+
+////Console.WriteLine(contaDoPedro.titular);
+////Console.WriteLine(contaDoPedro.agencia);
+////Console.WriteLine(contaDoPedro.conta);
+////Console.WriteLine(contaDoPedro.saldo);
+
+//////SAQUE E DEPOSITO
+
+////Console.WriteLine("Saldo da conta: " + contaDoAndre.saldo);
+
+////contaDoAndre.Depositar(150);
+////Console.WriteLine("Saldo Atualizado: " + contaDoAndre.saldo);
+
+////if (contaDoAndre.Sacar(400) == true)
+////{
+////    Console.WriteLine($"{contaDoAndre.titular} sacou R$ 100 ");
+////}
+////else
+////{
+////    Console.WriteLine("Saldo Insuficiente para Saque");
+////}
+
+////Console.WriteLine();
+////Console.WriteLine("Saldo Atualizado: " + contaDoAndre.saldo);
+
+
+////Console.ReadLine();
+
+
+//////metodo q exibe os dados do cliente
+////contaDaMaria.exibirInformações();
+
+
+//Cliente cliente = new Cliente();
+//cliente.nome = "Andre";
+//cliente.cpf = "123 123 123";
+//cliente.profissao = "Radiologista";
+
+//ContaCorrente contaDoAndre = new ContaCorrente();
+//contaDoAndre.titular = cliente;
+//contaDoAndre.agencia = 10;
 //contaDoAndre.conta = "1010-x";
 //contaDoAndre.saldo = 100;
 
-//Console.WriteLine($"Saldo Inicial de {contaDoAndre.titular} é R${contaDoAndre.saldo}");
+//Console.WriteLine("Nome do Titular: " + contaDoAndre.titular.nome);
+//Console.WriteLine("CPF do Titular: " + contaDoAndre.titular.cpf);
+//Console.WriteLine("Profissão: " + contaDoAndre.titular.profissao);
+//Console.WriteLine("N. Agencia: " + contaDoAndre.agencia);
+//Console.WriteLine("N. Conta: " + contaDoAndre.conta);
+//Console.WriteLine("Saldo: " + contaDoAndre.saldo);
 
-//// criando obj Maria
-//ContaCorrente contaDaMaria = new ContaCorrente();
+//ContaCorrente conta3 = new ContaCorrente(10, "0505-Z"); //Obrigado inserir agencia e conta porque a classe construtora requer parametro. Assim evitar
 
-//contaDaMaria.titular = "Maria";
-//contaDaMaria.agencia = 17;
-//contaDaMaria.conta = "1010-5";
-//contaDaMaria.saldo = 350;
+//conta3.SetSaldo(500);
+//conta3.Titular = new Cliente();
 
-//Console.WriteLine($"Saldo Inicial de {contaDaMaria.titular} é R${contaDaMaria.saldo}");
+//Console.WriteLine(conta3.GetSaldo());
+//Console.WriteLine(conta3.Agencia);
+//Console.WriteLine(conta3.Conta);
 
-//// Mtd para tranferir valores entre contas
-//contaDoAndre.Transferir(50, contaDaMaria);
+ContaCorrente conta4 = new ContaCorrente(123, "123-A");
+Console.WriteLine(ContaCorrente.TotalDeContas);
 
-//Console.WriteLine($"Saldo final da conta do Andre: {contaDoAndre.saldo}");
-//Console.WriteLine($"Saldo final da conta do Andre: {contaDaMaria.saldo}");
+ContaCorrente conta5 = new ContaCorrente(124, "124-A");
+Console.WriteLine(ContaCorrente.TotalDeContas);
 
-//// Para mostrar valores padrão do obj
-//ContaCorrente contaDoPedro = new ContaCorrente();
+ContaCorrente conta6 = new ContaCorrente(125, "125-A");
+Console.WriteLine(ContaCorrente.TotalDeContas);
 
-//Console.WriteLine(contaDoPedro.titular);
-//Console.WriteLine(contaDoPedro.agencia);
-//Console.WriteLine(contaDoPedro.conta);
-//Console.WriteLine(contaDoPedro.saldo);
-
-////SAQUE E DEPOSITO
-
-//Console.WriteLine("Saldo da conta: " + contaDoAndre.saldo);
-
-//contaDoAndre.Depositar(150);
-//Console.WriteLine("Saldo Atualizado: " + contaDoAndre.saldo);
-
-//if (contaDoAndre.Sacar(400) == true)
-//{
-//    Console.WriteLine($"{contaDoAndre.titular} sacou R$ 100 ");
-//}
-//else
-//{
-//    Console.WriteLine("Saldo Insuficiente para Saque");
-//}
-
-//Console.WriteLine();
-//Console.WriteLine("Saldo Atualizado: " + contaDoAndre.saldo);
-
-
-//Console.ReadLine();
-
-
-////metodo q exibe os dados do cliente
-//contaDaMaria.exibirInformações();
-
-
-Cliente cliente = new Cliente();
-cliente.nome = "Andre";
-cliente.cpf = "123 123 123";
-cliente.profissao = "Radiologista";
-
-ContaCorrente contaDoAndre = new ContaCorrente();
-contaDoAndre.titular = cliente;
-contaDoAndre.agencia = 10;
-contaDoAndre.conta = "1010-x";
-contaDoAndre.saldo = 100;
-
-Console.WriteLine("Nome do Titular: " + contaDoAndre.titular.nome);
-Console.WriteLine("CPF do Titular: " + contaDoAndre.titular.cpf);
-Console.WriteLine("Profissão: " + contaDoAndre.titular.profissao);
-Console.WriteLine("N. Agencia: " + contaDoAndre.agencia);
-Console.WriteLine("N. Conta: " + contaDoAndre.conta);
-Console.WriteLine("Saldo: " + contaDoAndre.saldo);
+ContaCorrente conta7 = new ContaCorrente(126, "126-A");
+Console.WriteLine(ContaCorrente.TotalDeContas);
